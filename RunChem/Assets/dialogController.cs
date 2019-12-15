48d214c04ba258f48186fa6b6c8d7c77ecb51c6e
dzkdani <33739691+dzkdani@users.noreply.github.com>
2019-12-15 15:24:00 +0700

    Revisi 3
    - Fixed Asset
    - Jalan
    - Main Menu BG
    - Title

---
+++ RunChem/Assets/dialogController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogController : MonoBehaviour
+{
+    public kalimat textKalimat;
+
+    public void StartDialogue()
+    {
+        FindObjectOfType<dialogManager>().SetDialogue(textKalimat);
+    }
+}
+
--- RunChem/Assets/dialogManager.cs
+++ RunChem/Assets/dialogManager.cs
@@ -1,18 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class dialogManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator profAnim;
+    public TextMeshProUGUI KalimatUIText; 
+    Queue<string> queKalimat = new Queue<string>();
+    public void SetDialogue(kalimat kalimat)
     {
-        
+         queKalimat.Clear();
+
+         foreach (string textKalimat in kalimat.daftarKalimat)
+         {
+             queKalimat.Enqueue(textKalimat);
+         }
+
+         DisplayNextText();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DisplayNextText()
     {
-        
+        if (queKalimat.Count == 0)
+        {
+            EndKalimat();
+            return;
+        }
+
+        profAnim.SetTrigger("EndDialog");
+
+        string tempKalimat = queKalimat.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSetterText(tempKalimat));
+    }
+
+    IEnumerator TypeSetterText(string kalimat)
+    {
+        KalimatUIText.text = "";
+        foreach (char huruf in kalimat.ToCharArray())
+        {
+            KalimatUIText.text += huruf;
+            yield return null;
+        }
+
+    }
+
+    private void EndKalimat()
+    {
+        sceneTransitionController.Instance.toNextLevel();
     }
 }
